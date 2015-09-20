c173fad824176d39f45dbe9ea2e7d52cfcf98cf3
sephthompson <sephthompson@gmail.com>
2015-09-20 16:13:20 -0400

    Test POST /question 2

--- src/main/java/com/devcru/arb/objects/QuestionWrapper.java
+++ src/main/java/com/devcru/arb/objects/QuestionWrapper.java
@@ -1,7 +1,10 @@
 package com.devcru.arb.objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class QuestionWrapper {
 	
+	@JsonProperty("question")
 	Question question;
 
 	public Question getQuestion() {
