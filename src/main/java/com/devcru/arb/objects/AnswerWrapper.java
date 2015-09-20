93e09278a7147bbb2b228c9ca5e4268b213613e7
sephthompson <sephthompson@gmail.com>
2015-09-20 14:38:53 -0400

    Test serializeObject 1

---
+++ src/main/java/com/devcru/arb/objects/AnswerWrapper.java
@@ -0,0 +1,15 @@
+package com.devcru.arb.objects;
+
+public class AnswerWrapper {
+	
+	Answer answer;
+
+	public Answer getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(Answer answer) {
+		this.answer = answer;
+	}
+
+}
---
+++ src/main/java/com/devcru/arb/objects/QuestionWrapper.java
@@ -0,0 +1,15 @@
+package com.devcru.arb.objects;
+
+public class QuestionWrapper {
+	
+	Question question;
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+}
