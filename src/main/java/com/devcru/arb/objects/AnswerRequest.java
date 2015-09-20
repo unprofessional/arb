8200d7937d1dc7638b32a42a1cce910f8d96b374
sephthompson <sephthompson@gmail.com>
2015-09-20 17:50:54 -0400

    Test POST /question 7

---
+++ src/main/java/com/devcru/arb/objects/AnswerRequest.java
@@ -0,0 +1,15 @@
+package com.devcru.arb.objects;
+
+public class AnswerRequest {
+	
+	String text = "";
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
---
+++ src/main/java/com/devcru/arb/objects/QuestionRequest.java
@@ -0,0 +1,15 @@
+package com.devcru.arb.objects;
+
+public class QuestionRequest {
+	
+	String text = "";
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
