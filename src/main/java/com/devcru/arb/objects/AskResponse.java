8d41ac96709706f89873a7495a128c99562a9f5b
sephthompson <sephthompson@gmail.com>
2015-09-20 20:12:46 -0400

    Test POST /question 13

---
+++ src/main/java/com/devcru/arb/objects/AskResponse.java
@@ -0,0 +1,15 @@
+package com.devcru.arb.objects;
+
+public class AskResponse {
+	
+	long id = 1L;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
